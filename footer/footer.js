39b417dd177d09004fb1509d53c6fe1a185ab6f1
Leibril <ariabi.sr@gmail.com>
2024-05-30 14:59:07 -0600

    creación del header y footer por Ariel

--- footer/footer.js
+++ footer/footer.js
@@ -1,3 +1,15 @@
 let footerDOM = document.querySelector(".footer");
 
-footerDOM.innerHTML = 'hola soy footer';
+footerDOM.innerHTML =  `
+
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s" alt="">    
+
+    <h2 class="titInt">Colaboradores</h2>
+
+    <div>
+        <a href="" class="userF">@Leibril2007</a>
+        <a href="" class="userF">@Marianaseven2013</a>
+        <a href="" class="userF">@Sofiagarcia358</a>
+        <a href="" class="userF">@keneth778</a>
+    </div>
+`;
