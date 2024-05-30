224e03a4ac97705a62b55e867f8515019b7af53f
marianaseven2013 <gmluch@scl.edu.gt>
2024-05-30 16:26:13 -0600

    Sección2 Génesis

--- section2/section2.js
+++ section2/section2.js
@@ -1,3 +1,86 @@
 let section2DOM = document.querySelector(".section2");
 
-section2DOM.innerHTML = 'hola soy section 2';
+section2DOM.innerHTML = `
+
+
+
+<h2>Etiquetas de Texto en HTML</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th>Etiqueta</th>
+            <th>Definición</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>a</td>
+            <td>Define un hipervínculo.</td>
+        </tr>
+        <tr>
+            <td>em</td>
+            <td>Texto enfatizado, generalmente mostrado en cursiva.</td>
+        </tr>
+        <tr>
+            <td>strong</td>
+            <td>Texto fuerte, generalmente mostrado en negrita.</td>
+        </tr>
+        <tr>
+            <td>cite</td>
+            <td>Define el título de una obra, generalmente mostrado en cursiva.</td>
+        </tr>
+        <tr>
+            <td>code</td>
+            <td>Define un fragmento de código de computadora.</td>
+        </tr>
+        <tr>
+            <td>mark</td>
+            <td>Texto resaltado o marcado.</td>
+        </tr>
+        <tr>
+            <td>q</td>
+            <td>Define una cita corta en línea.</td>
+        </tr>
+        <tr>
+            <td>small</td>
+            <td>Texto de menor tamaño.</td>
+        </tr>
+        <tr>
+            <td>abbr</td>
+            <td>Define una abreviatura o acrónimo.</td>
+        </tr>
+        <tr>
+            <td>h1</td>
+            <td>Define el tramaño de la letra más utilizado para encabezados</td>
+        </tr>
+        <tr>
+            <td>del</td>
+            <td>Texto que ha sido eliminado, generalmente mostrado con una línea a través del texto.</td>
+        </tr>
+        <tr>
+            <td>ins</td>
+            <td>Texto que ha sido insertado, generalmente mostrado subrayado.</td>
+        </tr>
+        <tr>
+            <td>sup</td>
+            <td>Texto en superíndice.</td>
+        </tr>
+        <tr>
+            <td>sub</td>
+            <td>Texto en subíndice.</td>
+        </tr>
+        <tr>
+            <td>br</td>
+            <td>Salto de línea.</td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+    
+
+
+
+`;
