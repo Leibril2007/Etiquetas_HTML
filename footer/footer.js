cdad050000e0fbc1d118c1672610e73c0a9e0a26
Leibril <ariabi.sr@gmail.com>
2024-05-30 19:23:20 -0600

    colocación de links de github

--- footer/footer.js
+++ footer/footer.js
@@ -4,12 +4,12 @@ footerDOM.innerHTML =  `
 
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s" alt="">    
 
-    <h2 class="titInt">Colaboradores</h2>
+    <h3 class="titInt">Colaboradores</h3>
 
     <div>
-        <a href="" class="userF">@Leibril2007</a>
-        <a href="" class="userF">@Marianaseven2013</a>
-        <a href="" class="userF">@Sofiagarcia358</a>
-        <a href="" class="userF">@keneth778</a>
+        <a href="https://github.com/Leibril2007/Etiquetas_HTML" class="userF">@Leibril2007</a>
+        <a href="https://github.com/marianaseven2013/Etiquetas_html" class="userF">@Marianaseven2013</a>
+        <a href="https://github.com/sofiagarcia358/etiquetas_html" class="userF">@Sofiagarcia358</a>
+        <a href="https://github.com/keneth778/etiquetashtml" class="userF">@keneth778</a>
     </div>
 `;
